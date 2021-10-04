74b132b9fc4ea61ddf268cf1b0f862853ace3881
padma-pani-patra <padmapanipatra@gmail.com>
2021-10-04 16:46:06 +0530

    exported S3

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const base64SingleUpload = require('./base64FileUpload/base64SingleUpload');
+const { uploadSingleFileS3, uploadMultipleFileS3, getFileStream } = require('./s3FileUpload/s3')
 
 module.exports = {
   base64SingleUpload,
+  uploadSingleFileS3,
+  uploadMultipleFileS3,
+  getFileStream,
 };
