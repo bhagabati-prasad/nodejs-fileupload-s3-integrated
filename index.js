efd16c77104ef2beccb9492e23585ef616226f28
padma-pani-patra <padmapanipatra@gmail.com>
2021-10-05 10:02:00 +0530

    added the required file for express file upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const base64SingleUpload = require('./base64FileUpload/base64SingleUpload');
 const { uploadSingleFileS3, uploadMultipleFileS3, getFileStream } = require('./s3FileUpload/s3')
+const multiFileUpload = require('./expressFileupload/multiFileUpload')
+const singleFileUpload = require('./expressFileupload/singleFileUpload')
 
 module.exports = {
   base64SingleUpload,
   uploadSingleFileS3,
   uploadMultipleFileS3,
   getFileStream,
+  multiFileUpload,
+  singleFileUpload,
 };
